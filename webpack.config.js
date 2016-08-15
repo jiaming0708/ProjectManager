3127303471f41133493846c29a9616c2f671d43c
Jimmy <jiaming0708@gmail.com>
2016-08-15 09:18:20 +0800

    fix the bug that scss output two files (js/css)

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require("path");
 
+
 module.exports = {
   entry: {
     vendor: './src/vendor.ts',
-    app: './src/main.ts',
-    style: './scss/main.scss'
+    app: './src/main.ts'
   },
   output: {
     path: __dirname,
     filename: './dist/[name].bundle.js'
   },
   resolve: {
-    extensions: ['', '.ts', '.js', '.scss', '.css']
+    extensions: ['', '.ts', '.js']
   },
   devtool: 'source-map',
   module: {
@@ -21,18 +21,18 @@ module.exports = {
       {
         test: /\.ts/,
         loaders: ['ts-loader'],
-        include: ['./src/']
+        exclude: /node_modules/
       },{
         test: /\.css$/,
         loader: 'style!css'
       },{
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+        loader: ExtractTextPlugin.extract(['css', 'sass'])
       }
     ]
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js"),
-    new ExtractTextPlugin("./dist/[name].css")
+    new ExtractTextPlugin("./dist/style.css")
   ]
 }
