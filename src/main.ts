e087b898bf8625b2ae1c20e15847510d5feaeade
Jimmy <jiaming0708@gmail.com>
2016-08-24 14:20:34 +0800

    upgrade to rc5

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic }    from '@angular/platform-browser-dynamic';
+
 import { AppComponent } from './app.component';
-import '../scss/main.scss'
-bootstrap(AppComponent);
+import { routing, appRoutingProviders } from './app.route';
+import { CreateComponent } from './create/create.component';
+import { DashBoardComponent } from './dashboard/dashboard.component';
+
+import '../scss/main.scss';
+
+@NgModule({
+  imports: [BrowserModule, routing],
+  declarations: [AppComponent, CreateComponent, DashBoardComponent],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
