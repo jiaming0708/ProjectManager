c84a1ad4219bb80d00c8306cf5e8cbdf5032b397
Jimmy <jiaming0708@gmail.com>
2016-09-09 17:26:52 +0800

    add html to the component

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Step } from '../data/step.data';
+import { Item } from '../data/item.data';
+import { StepService } from '../data/step.service';
+
 @Component({
   selector: 'dashboard',
-  template: `
-  dashboard
-  `
+  template: require('./dashboard.template.html'),
+  providers: [StepService]
 })
-export class DashBoardComponent { }
+export class DashBoardComponent implements OnInit {
+  StepList: Step[];
+
+  constructor(private _stepService: StepService) { }
+
+  ngOnInit(): void {
+    this._stepService.getInitialStepList()
+      .then(data => this.StepList = data);
+  }
+}
